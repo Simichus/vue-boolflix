6154fecb6cb5da0becf16d1bc7853a8cc6a8bdda
Simichus <simonefilippobilardo@gmail.com>
2021-09-10 16:18:15 +0200

    Aggiunto header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <SearchMovie @startResearch="startResearch"/>
+  </header>
+</template>
+
+<script>
+import SearchMovie from '@/components/SearchMovie.vue'
+export default {
+name: 'Header',
+components:{
+    SearchMovie
+},
+methods:{
+    startResearch(searchTerm){
+        return this.$emit('startResearch', searchTerm)
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SearchMovie.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <input type="text" v-model="searchMovie" placeholder="Cerca un film..." @keyup.enter="startResearch">
+      <button @click="startResearch">Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'SearchMovie',
+data(){
+    return{
+        searchMovie:''
+    }
+},
+methods:{
+    startResearch(){
+        if (!this.searchMovie) return
+        return this.$emit('startResearch', this.searchMovie)
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
