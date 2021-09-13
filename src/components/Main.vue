6822e8c35fa870c757752e02bbf9194b4aa8b2a1
Simichus <simonefilippobilardo@gmail.com>
2021-09-13 09:36:34 +0200

    errore in axios.get

---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+      <ShowResult result-type="movies" :search-term="searchTerm"/>
+      <ShowResult result-type="tv series" :search-term="searchTerm"/>
+  </section>
+</template>
+
+<script>
+import ShowResult from '@/components/ShowResult.vue'
+export default {
+name:'Main',
+components:{
+    ShowResult
+},
+props:['searchTerm'],
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ShowResult.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+      <h1>{{resultType}}</h1>
+  </section>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+name:'ShowResult',
+props:['resultType', 'searchTerm'],
+data(){
+    return{
+       baseURI:'https://api.themoviedb.org/3/',
+       apiKey:'94e21c35f1a4303de4bd1504d52ad385'
+    }
+},
+computed:{
+    // results(){
+    //     let results = [];
+    //     axios.get(`https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs`).then(res => {
+    //      results = [...res.data];
+    //     })  
+    //     return results
+    // },
+    type(){
+        if (this.resultType === 'movies') return 'search/movie/';
+        if (this.resultType === 'tv series') return 'search/tv/';
+        else return ''
+    }
+},
+methods:{
+    
+}
+
+}
+</script>
+
+<style scoped lang="scss">
+h1{
+    text-transform: uppercase;
+}
+</style>
